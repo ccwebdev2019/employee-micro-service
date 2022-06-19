6ec49a9b91586f9e6c6695ed4c0619402f6adb8e
allthingscode2022 <allthingscode2022@gmail.com>
2022-06-18 19:24:13 -0500

    employee model and db table

---
+++ src/main/java/com/demo/employee/models/Employee.java
@@ -0,0 +1,42 @@
+package com.demo.employee.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private String city;
+	private String state;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
